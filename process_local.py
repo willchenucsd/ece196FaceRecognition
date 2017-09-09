04c73d758401b91199b28e7a861462ef73b3cd2e
Will Chen <yuc143@ucsd.edu>
2017-09-09 10:37:29 -0700

    process local image

--- process_local.py
+++ process_local.py
@@ -3,9 +3,9 @@ Author: W Chen
 Prerequisite: You need to install OpenCV before running this code
 The code here is an example of what you can write to print out 'Hello World!'
 Now modify this code to process a local image and do the following:
-1. read image
+1. read geisel-hr.jpg
 2. convert color to gray scale
-3. resize to 100x100 dimensions
+3. resize to half of its original dimensions
 4. draw a box at the center the image with size 10x10
 5. save image with a new name to local directory
 All the above steps should be in one function called process_image()
